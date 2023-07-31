144dd76498f402cbe1449d607b459fc42c9b713a
ygautomo <y_gautomo@yahoo.com>
2023-07-31 18:12:47 +0700

    Update database.py

--- api/app/database.py
+++ api/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import databases
 #%%
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:prospera2023@139.180.145.25:5432/hkpd"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:xxxx@xxx.xxx.xxx.xx:5432/hkpd"
 database = databases.Database(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
